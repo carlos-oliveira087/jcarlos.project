5ee7b19c7aaba4cfc8aae25c2b6190f32af5c49e
Carlos Oliveira <carlinhos.oneto0@gmail.com>
2024-12-27 00:35:16 -0300

    feat: aplicações necessárias instaladas

---
+++ Jcarlos_project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center bg-slate-400 text-white p-4">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+
+
+
+export default App
